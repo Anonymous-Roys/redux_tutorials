994d99d8e0d0bfd79589ffaf1c0c8b745954e0a7
David Arhin <davidarhin2005@gmail.com>
2024-04-19 22:35:29 +0000

    icecream state

--- index.js
+++ index.js
@@ -1,26 +1,46 @@
+const redux = require('redux')
+const createStore = redux.createStore;
+
 const BUY_CAKE = "BUY_CAKE";
+const BUY_ICECREAM = "BUY_ICECREAM";
 
 //action creator
-function buycake(){
-
+function buyCake(){
     return {
         type: BUY_CAKE,
+        info:"first redux action"
+    }
+}
+function buyIceCream(){
+    return {
+        type: BUY_ICECREAM,
         info:"second redux action"
     }
 }
-
-//reducer
-// (previousState, action) => newState
-
-const initialState={
+//------------STATE-----------
+// const initialState={
+//     numOfCakes:10,
+//     numOfIceCreams:20
+// }
+const initialCakes = {
     numOfCakes:10
 }
+const initialIceCream = {
+    numOfIceCreams:20
+}
 
+
+//------------------REDUCER-----------------
+// (previousState, action) => newState
 const reducer = (state=initialState, action)=> {
     switch(action.type){
         case BUY_CAKE: return{
             ...state,   //copying the state object
             numOfCakes: state.numOfCakes -1
+        }
+        case BUY_ICECREAM: return{
+            ...state,   //copying the state object
+            numOfIceCreams: state.numOfIceCreams -1
         }
 
         default:
@@ -29,3 +49,20 @@ const reducer = (state=initialState, action)=> {
 }
 
 //stores
+//holding application state
+const store = createStore(reducer);
+
+//allows access to state to be updated using the getState
+console.log('Initial state', store.getState());
+
+
+//subscribing to the
+const unsubscribe = store.subscribe(()=>console.log('Updated State', store.getState()));
+
+//dispatch takes action a parameter allowing the state to be updated
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
+unsubscribe()
